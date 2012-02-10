03db16664e99ac5f5a180d759554397e668c26b6
Jake Brukhman <jbrukh@gmail.com>
2012-02-09 20:17:15 -0500

    Split into two files.

--- pkg/gplot.go
+++ pkg/gplot.go
@@ -14,35 +14,12 @@ slightly more appropriate for high-performance plotting.
 
 import (
 	"fmt"
-	"io"
 	"os"
     "exec"
-    "encoding/binary"
 )
 
 // the system command for gnuplot
 var gnuplot string
-
-// conn is the structure
-// that represents the connection to gnuplot
-type conn struct {
-	handle *exec.Cmd
-	stdin  io.WriteCloser
-}
-
-// create a new conn
-func newConn(persist bool) (*conn, os.Error) {
-    args := make([]string, 0, 10)
-	if persist {
-		args = append(args, "-persist")
-	}
-	cmd := exec.Command(gnuplot, args...)
-	stdin, err := cmd.StdinPipe()
-	if err != nil {
-		return nil, err
-	}
-	return &conn{cmd, stdin}, cmd.Start()
-}
 
 // resolve the gnuplot command on this
 // system, or panic if it is not available
@@ -55,68 +32,11 @@ func init() {
     }
 }
 
-func (self *Plotter) PlotX(data []float64, title string) (err os.Error) {
-    const plotCmd = "plot \"-\" binary array=%d title \"%s\" with %s"
-    line := fmt.Sprintf(plotCmd, len(data), title, self.style)
-    err = self.cmd(line)
-    if err != nil {
-        return
-    }
-    err = binary.Write(self.conn.stdin, binary.BigEndian, data)
-    if err != nil {
-          return
-    }
-    return
-}
-
-func (self *Plotter) SetStyle(style string) {
-    // TODO: check validity
-    self.style = style
-}
-
-
-
 
 type Plotter struct {
 	conn     *conn
 	debug    bool
 	style    string // current plotting style
-}
-
-func (self *Plotter) cmd(format string, a ...interface{}) os.Error {
-	cmd := fmt.Sprintf(format, a...) + "\n"
-	n, err := io.WriteString(self.conn.stdin, cmd)
-
-	if self.debug {
-		fmt.Printf("cmd$ %v", cmd)
-		fmt.Printf("cmd$ <%v\n", n)
-	}
-
-	return err
-}
-
-func (self *Plotter) CheckedCmd(format string, a ...interface{}) {
-	err := self.cmd(format, a...)
-	if err != nil {
-		str := fmt.Sprintf("error: %v\n", err)
-		panic(str)
-	}
-}
-
-func (self *Plotter) Close() (err os.Error) {
-	if self.conn != nil && self.conn.handle != nil {
-		self.conn.stdin.Close()
-		err = self.conn.handle.Wait()
-	}
-	return err
-}
-
-func (self *Plotter) SetXLabel(label string) os.Error {
-	return self.cmd(fmt.Sprintf("set xlabel '%s'", label))
-}
-
-func (self *Plotter) SetYLabel(label string) os.Error {
-	return self.cmd(fmt.Sprintf("set ylabel '%s'", label))
 }
 
 func NewPlotter(persist, debug bool) (plotter *Plotter, err os.Error) {
@@ -128,6 +48,48 @@ func NewPlotter(persist, debug bool) (plotter *Plotter, err os.Error) {
 		return nil, err
 	}
 	p.conn = conn
-    p.cmd(defaults)
+    p.conn.cmd(defaults)
 	return p, nil
 }
+
+func (p *Plotter) PlotX(data []float64, title string) (err os.Error) {
+    const plotCmd = "plot \"-\" binary array=%d title \"%s\" with %s"
+    line := fmt.Sprintf(plotCmd, len(data), title, p.style)
+    err = p.conn.cmd(line)
+    if err != nil {
+        return
+    }
+    p.conn.data(data)
+    if err != nil {
+          return
+    }
+    return
+}
+
+func (p *Plotter) SetStyle(style string) {
+    // TODO: check validity
+    p.style = style
+}
+
+
+
+
+
+
+func (p *Plotter) CheckedCmd(format string, a ...interface{}) {
+	err := p.conn.cmd(format, a...)
+	if err != nil {
+		str := fmt.Sprintf("error: %v\n", err)
+		panic(str)
+	}
+}
+
+
+func (p *Plotter) SetXLabel(label string) os.Error {
+	return p.conn.cmd(fmt.Sprintf("set xlabel '%s'", label))
+}
+
+func (p *Plotter) SetYLabel(label string) os.Error {
+	return p.conn.cmd(fmt.Sprintf("set ylabel '%s'", label))
+}
+
